a28891d20e51d670c741e76da1be11ae6da922dd
Angel Gonzalez <aigonzal@usc.edu>
2019-03-14 12:15:17 -0700

    Create base class for Sampler
    This could be useful in the future when using other data sets.

--- mnist_sampler.py
+++ mnist_sampler.py
@@ -1,8 +1,10 @@
 import numpy as np
 from tensorflow.keras.datasets import mnist
 
+from sampler import Sampler
 
-class MnistSampler:
+
+class MnistSampler(Sampler):
     def __init__(self, training_digits=None, batch_size=None):
         """Set the parameters to sample from MNIST.
 
@@ -21,46 +23,16 @@ class MnistSampler:
         else:
             self._training_digits = list(range(10))
 
-        (self._x_train, self._y_train), (self._x_test,
-                                         self._y_test) = mnist.load_data()
-        self._x_train = self._x_train / 255.0
-        self._x_test = self._x_test / 255.0
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        x_train = x_train / 255.0
+        x_test = x_test / 255.0
 
         for i in range(10):
             if i not in self._training_digits:
-                remove_train_indices = np.where(self._y_train == i)[0]
-                self._x_train = np.delete(
-                    self._x_train, remove_train_indices, axis=0)
-                self._y_train = np.delete(
-                    self._y_train, remove_train_indices, axis=0)
-                remove_test_indices = np.where(self._y_test == i)[0]
-                self._x_test = np.delete(
-                    self._x_test, remove_test_indices, axis=0)
-                self._y_test = np.delete(
-                    self._y_test, remove_test_indices, axis=0)
-
-        self._N = self._x_train.shape[0]
-        assert batch_size is None or 0 <= batch_size <= self._N
-        if batch_size:
-            self._batch_size = batch_size
-            self._sample_indices = np.array(range(0, self._N))
-        else:
-            self._batch_size = self._N
-
-    def sample(self):
-        if self._batch_size < self._N:
-            end_of_epoch = False
-            if self._sample_indices.size < self._batch_size:
-                indices = self._sample_indices
-                self._sample_indices = np.array(range(0, self._N))
-                end_of_epoch = True
-            else:
-                indices = np.random.randint(0, len(self._sample_indices),
-                                            self._batch_size)
-                self._sample_indices = np.delete(self._sample_indices, indices)
-            return self._x_train[indices], self._y_train[indices], end_of_epoch
-        else:
-            return self._x_train, self._y_train, True 
-
-    def get_test_set(self):
-        return self._x_test, self._y_test
+                remove_train_indices = np.where(y_train == i)[0]
+                x_train = np.delete(x_train, remove_train_indices, axis=0)
+                y_train = np.delete(y_train, remove_train_indices, axis=0)
+                remove_test_indices = np.where(y_test == i)[0]
+                x_test = np.delete(x_test, remove_test_indices, axis=0)
+                y_test = np.delete(y_test, remove_test_indices, axis=0)
+        super().__init__(x_train, y_train, x_test, y_test, batch_size)
